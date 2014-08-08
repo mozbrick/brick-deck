71f6a2088e7334dcd730acc1b657318c00139c70
nicolagreco <notsecurity@gmail.com>
2014-08-07 22:07:40 -0700

    adding the template id

--- src/deck.js
+++ src/deck.js
@@ -101,7 +101,7 @@
   BrickDeckElementPrototype.attachedCallback = function() {
 
     var importDoc = currentScript.ownerDocument;
-    var template = importDoc.querySelector('template');
+    var template = importDoc.querySelector('#brick-deck-template');
 
     // fix styling for polyfill
     if (Platform.ShadowCSS) {
